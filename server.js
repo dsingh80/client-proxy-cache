ff6838a88bbb5f3de0a4482be97ab16f9fa4f7ce
Damanveer Singh <d.singh_27@yahoo.com>
2022-01-20 13:18:53 -0500

    Added basic server;

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+'use strict';
+
+const express = require('express'),
+  bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log(req.url);
+  console.log(req.body);
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
